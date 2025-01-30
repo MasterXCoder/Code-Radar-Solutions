000e61907c5babed5471ca6955e62101f695ff3d
Vansh Singh <vansh261105@gmail.com>
2025-01-30 10:33:50 +0530

    Solution to Grade Based on Marks

---
+++ Grade Based on Marks@@6788dd63490637cfc44e1b96/code.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    char s;
+    s = (a>=90)? 'A':
+        (a>=80)? 'B':
+        (a>=70)? 'C':
+        (a>=60)? 'D':'F';
+        //Terinary operator
+    printf("%c",s);
+    return 0;
+}
