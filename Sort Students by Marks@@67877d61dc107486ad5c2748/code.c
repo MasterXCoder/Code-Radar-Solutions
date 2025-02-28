f290df2026a07278db5c995bf1cb3951168467ed
Vanshdeep Singh Dhot <vansh261105@gmail.com>
2025-02-28 20:42:26 +0530

    Solution to Sort Students by Marks

---
+++ Sort Students by Marks@@67877d61dc107486ad5c2748/code.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+void bubbleSort(int s[], int n) {
+    int t;
+    for (int i = 0; i < n-1; i++) {
+        for (int j = 0; j < n-i-1; j++) {
+            if (s.mark[j] > s.mark[j+1]) {
+                t = s.mark[j];
+                s.mark[j] = s.mark[j+1];
+                s.mark[j+1] = t;
+            }
+        }
+    }
+}
+
+int main(){
+    struct data{
+        int roll;
+        char name[25];
+        float mark;
+    };
+    int n;
+    scanf("%d",&n);
+    struct data s[n];
+    for(int i=0; i<n; i++){
+        scanf("%d %s %f",&s[i].roll,&s[i].name,&s[i].mark);
+    }
+    int check;
+    int r = 0; 
+    scanf("%d",&check);
+    for(int i=0; i<n; i++){
+        printf("%d %s %f",&s[i].roll,&s[i].name,&s[i].mark);
+    }
+    return 0;
+}
