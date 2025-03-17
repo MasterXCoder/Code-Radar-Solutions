fa0bd0ff29e595c26c7dcbf2d7959f80a52d544e
Vanshdeep Singh Dhot <vansh261105@gmail.com>
2025-03-17 20:10:51 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 int f=0,f1=0,f2=1;
 
-int fibo(int n){
+int fibonacciSeries(int n){
     if(n>0){
-        printf("%d, ",f);
+        printf("%d ",f);
         f=f1+f2;
         f2=f1;
         f1=f;
