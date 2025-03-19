9fa80b035c65ce3e2d0e3c4772da37df84efc7c8
Vanshdeep Singh Dhot <vansh261105@gmail.com>
2025-03-19 13:18:02 +0530

    Solution to Toll Plaza Management System

---
+++ Toll Plaza Management System@@67877d61dc107486ad5c2750/code.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+struct data {
+    char number[25];
+    char name[25];
+    float amt;
+};
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    struct data s[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%s %s %f", &s[i].number, s[i].name, &s[i].amt);
+    }
+    int c=0,t=0,b=0;
+    for (int i = 0; i < n; i++) {
+       s[i].name=="Car"? c++:name=="Truck"? t++:b++;
+    }
+    printf("Cars: %.2f, Trucks: %.2f, Bikes: %.2f\n");
+    return 0;
+}
