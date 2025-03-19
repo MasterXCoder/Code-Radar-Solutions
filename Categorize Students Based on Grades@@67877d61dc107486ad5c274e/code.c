b1fffd7d31a16630dab0468ef7d129a4f2f34b58
Vanshdeep Singh Dhot <vansh261105@gmail.com>
2025-03-19 12:57:49 +0530

    Solution to Categorize Students Based on Grades

---
+++ Categorize Students Based on Grades@@67877d61dc107486ad5c274e/code.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+struct data {
+    int roll;
+    char name[25];
+    float mark;
+};
+
+char res(float mark){
+    mark>=85? 'A':mark>=70? 'B':'C';
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    struct data s[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d %s %f", &s[i].roll, s[i].name, &s[i].mark);
+    }
+
+    for (int i = n; i >=0; i--) {
+        printf("Roll Number: %d, Name: %s, MArks: %.2f\n", s[i].roll, s[i].name, char(s[i].mark));
+    }
+
+    return 0;
+}
