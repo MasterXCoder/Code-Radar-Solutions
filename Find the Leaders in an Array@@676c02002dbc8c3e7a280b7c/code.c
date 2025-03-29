5581617e8e28498b6d9e18c7b9e3a87d3ca87966
Vanshdeep Singh Dhot <vansh261105@gmail.com>
2025-03-29 20:15:31 +0530

    Solution to Find the Leaders in an Array

--- Find the Leaders in an Array@@676c02002dbc8c3e7a280b7c/code.c
+++ Find the Leaders in an Array@@676c02002dbc8c3e7a280b7c/code.c
@@ -1,49 +1,26 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-// Bubble Sort to sort the array
-void bubbleSort(int arr[], int n) {
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                int temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
-        }
-    }
-}
 
 int main() {
     int n;
     scanf("%d", &n);
-
-    if (n < 2) {
-        printf("-1");
-        return 0;
-    }
-
     int arr[n];
+    
     for (int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
 
-    // Sort the array using bubble sort
-    bubbleSort(arr, n);
-
-    int minDiff = abs(arr[1] - arr[0]);
-    int val1 = arr[0], val2 = arr[1];
-
-    // Find the pair with the minimum difference
-    for (int i = 1; i < n - 1; i++) {
-        int diff = abs(arr[i + 1] - arr[i]);
-        if (diff < minDiff) {
-            minDiff = diff;
-            val1 = arr[i];
-            val2 = arr[i + 1];
+    for (int i = 0; i < n; i++) {
+        int isLeader = 1;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[i] < arr[j]) {
+                isLeader = 0;
+                break;
+            }
+        }
+        if (isLeader) {
+            printf("%d ", arr[i]);
         }
     }
 
-    printf("%d %d", val1, val2);
     return 0;
 }
