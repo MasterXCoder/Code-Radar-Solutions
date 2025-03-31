1690bfb08486cf588b29258bac5e17977421f3da
Vanshdeep Singh Dhot <vansh261105@gmail.com>
2025-03-31 19:00:27 +0530

    Solution to Find the Second Smallest Element in an Array

--- Find the Second Smallest Element in an Array@@676c02002dbc8c3e7a280b85/code.c
+++ Find the Second Smallest Element in an Array@@676c02002dbc8c3e7a280b85/code.c
@@ -13,33 +13,36 @@ void bubbleSort(int arr[], int n) {
     }
 }
 
-int main(){
-    int n,c=0,val=0;
+int main() {
+    int n, c = 0, val = 0;
     scanf("%d", &n);
-    int arr[n], res[c];
-    for(int i=0; i<n; i++){
+    int arr[n], res[n]; // Correctly declare res with size n
+
+    for (int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
-    for(int i=0; i<n; i++){
-        val=1;
-        for(int j=i; j<c; j++){
-            if(arr[i]==arr[j]){
-                val=0;
+
+    // Extract unique values
+    for (int i = 0; i < n; i++) {
+        val = 1;
+        for (int j = 0; j < c; j++) { // Corrected loop for checking duplicates
+            if (arr[i] == res[j]) {
+                val = 0;
+                break;
             }
         }
-        if(val){
-            res[c]=arr[i];
+        if (val) {
+            res[c] = arr[i];
             c++;
         }
     }
-    bubbleSort(res, c);
-    int small=res[1];
-    for(int i=1; i<n; i++){
-        if(res[i]!=res[i-1]){
-            printf("%d",small);
-            return 0;
-        }
+
+    if (c < 2) {
+        printf("-1\n"); // Less than two unique values
+        return 0;
     }
-    printf("-1");    
+
+    bubbleSort(res, c);
+    printf("%d\n", res[1]); // Second lowest unique value
     return 0;
 }
