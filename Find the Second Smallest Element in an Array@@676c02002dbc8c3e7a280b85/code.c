db4b0a1172de104ade579ff783e911b096c79fee
Vanshdeep Singh Dhot <vansh261105@gmail.com>
2025-03-31 18:58:19 +0530

    Solution to Find the Second Smallest Element in an Array

--- Find the Second Smallest Element in an Array@@676c02002dbc8c3e7a280b85/code.c
+++ Find the Second Smallest Element in an Array@@676c02002dbc8c3e7a280b85/code.c
@@ -39,9 +39,7 @@ int main(){
             printf("%d",small);
             return 0;
         }
-        else{
-            printf("-1");    
-            return 0;
-        }
     }
+    printf("-1");    
+    return 0;
 }
